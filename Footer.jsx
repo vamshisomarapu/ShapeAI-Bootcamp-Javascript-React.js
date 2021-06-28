456ef7f19420f7441e3f60fa61216a6bb0097a00
vamshisomarapu <55593478+vamshisomarapu@users.noreply.github.com>
2021-06-28 15:47:55 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,12 @@
+function Footer(){
+  return(
+    
+      <footer>
+        <p>Copyright by Shape AI @ {new Date(). getFullYear()}</p>
+        </footer>
+      
+  );
+}
+
+
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js</h1>
+      <p>This was an amazing Bootcamp taken by Shaurya Sinha sir.We covered everything from scratch including javascript,React.js,HTML, CSS.Finally I loved the Bootcamp and i am looking forward to attend many more.</p>
+    </div>
+  );
+}
+
+export default Info;
